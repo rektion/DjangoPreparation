1ee499cd658b61ea248ca5919df906b0ed22bd6b
rektion <redutilos0@gmail.com>
2020-03-03 09:14:15 +0100

    test 404

--- root/crepes_bretonnes/blog/views.py
+++ root/crepes_bretonnes/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+def view_article(request, id_article):
+    # Si l'ID est supérieur à 100, nous considérons que l'article n'existe pas
+    if id_article > 100:
+        raise Http404
+
+    return HttpResponse('<h1>Mon article ici</h1>')
