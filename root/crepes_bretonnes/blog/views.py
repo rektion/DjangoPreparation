387ab2096d089a6b6a8280515c5246a919b5f168
rektion <redutilos0@gmail.com>
2020-03-26 19:41:58 +0100

    ImportError: cannot import name views

--- root/crepes_bretonnes/blog/views.py
+++ root/crepes_bretonnes/blog/views.py
@@ -1,39 +1,21 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import redirect
+from django.http import Http404
 from django.shortcuts import render
-from datetime import datetime
+from blog.models import Article
+from django.shortcuts import render, get_object_or_404
 
-def date_actuelle(request):
-    return render(request, 'blog/date.html', {'date': datetime.now()})
+def lire(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, 'blog/lire.html', {'article':article})
 
-
-def addition(request, nombre1, nombre2):    
-    total = nombre1 + nombre2
-
-    # Retourne nombre1, nombre2 et la somme des deux au tpl
-    return render(request, 'blog/addition.html', locals())
-	
 def accueil(request):
-	return render(request,'blog/accueil.html')
+    """ Afficher tous les articles de notre blog """
+    articles = Article.objects.all() # Nous sélectionnons tous nos articles
+    return render(request, 'blog/accueil.html', {'derniers_articles': articles})
 
-def list_articles(request, month, year):
-    """ Liste des articles d'un mois précis. """
-    return HttpResponse(
-        "Vous avez demandé les articles de {0} {1}.".format(month, year)  
-    )
-
-def view_article(request, id_article):
-    if id_article > 100:
+def lire(request, id):
+    try:
+        article = Article.objects.get(id=id)
+    except Article.DoesNotExist:
         raise Http404
 
-    return redirect(view_redirection)
-
-def view_redirection(request):
-    return HttpResponse("Vous avez été redirigé.")
-
-def home(request):
-    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Bienvenue sur mon blog !</h1>
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
-    """)
+    return render(request, 'blog/lire.html', {'article': article})
